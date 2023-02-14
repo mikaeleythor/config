6bf336545238a98e0e8f611b94d131a5dd0fe832
mikaeleythor <eythor108@gmail.com>
2023-02-14 09:02:16 +0000

    Fix launch so bolybar restarts on config changes

--- polybar/scripts/launch.sh
+++ polybar/scripts/launch.sh
@@ -2,7 +2,7 @@
 
 killall -q polybar
 
-polybar -c $HOME/.config/polybar/config main 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -r -c $HOME/.config/polybar/config main 2>&1 | tee -a /tmp/polybar.log & disown
 if [[ "$( connected-DVI-monitors )" = DVI* ]]; then
     polybar -c $HOME/.config/polybar/config secondary 2>&1 | 
         tee -a /tmp/polybar.log & disown;
